7d67354a7090eab926abc3eb5cc66b1f3fcd9987
John Helder <johnhelderjp@gmail.com>
2024-01-02 22:47:25 -0300

    Pegando os dados do json de forma dinamica

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const configdgv={
   endpoint:"produtos.json",
+  idDestino:"dvgDados",
 }
 const dgv=(configdgv=>{
+  const dvgDados = document.getElementById(configdgv.idDestino);
   fetch(configdgv.endpoint)
   .then(res=>res.json())
   .then(res=>{
@@ -13,7 +15,28 @@ const dgv=(configdgv=>{
       c1.setAttribute("class", "c1");
       c1.innerHTML = el.id;
       dgvLinha.appendChild(c1);
+
+      const c2 = document.createElement("div");
+      c1.setAttribute("class", "c2");
+      c2.innerHTML = el.produto;
+      dgvLinha.appendChild(c2);
+
+      const c3 = document.createElement("div");
+      c3.setAttribute("class", "c3");
+      c3.innerHTML = el.marca;
+      dgvLinha.appendChild(c3);
+
+      const c4 = document.createElement("div");
+      c4.setAttribute("class", "c4");
+      c1.innerHTML = el.modelo;
+      dgvLinha.appendChild(c4);
+
+      const c5 = document.createElement("div");
+      c5.setAttribute("class", "c5");
+      c5.innerHTML = "DEV";
+      dgvLinha.appendChild(c5);
+
+      dvgDados.appendChild(dgvLinha);
     });
-// console.log(res);
   });
 }
